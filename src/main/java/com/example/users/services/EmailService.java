1e41c74053b5ab1065b39a196d5eb6be34341de6
Daniel <danielima.projex@gmail.com>
2025-03-24 13:50:43 -0400

    feat: add email service

---
+++ src/main/java/com/example/users/services/EmailService.java
@@ -0,0 +1,34 @@
+package com.example.users.services;
+import com.resend.*;
+import com.resend.core.exception.ResendException;
+import com.resend.services.emails.model.CreateEmailOptions;
+import com.resend.services.emails.model.CreateEmailResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private String SECRET_KEY = "re_GVMa9mfT_GfAS8tLZ6fzFcCBjYaoPJ165";
+    @Value("${EMAIL}")
+    private String EMAIL_FROM;
+
+    private Resend resend = new Resend(SECRET_KEY);
+
+    public void sendEmail(String to, String subject, String body) {
+        CreateEmailOptions params = CreateEmailOptions.builder()
+                .from(EMAIL_FROM)
+                .to(to)
+                .subject(subject)
+                .html(body)
+                .build();
+
+        try {
+            CreateEmailResponse data = resend.emails().send(params);
+            System.out.println(data);
+        } catch (ResendException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
